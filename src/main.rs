46a1f2538b48ffdb883950a3eafa3732942343fa
Oluwafemi Akinwa <kaiser.phemi@protonmail.ch>
2026-06-12 23:06:56 +0100

    chore()

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
-
 fn main() {
-    // Arrays
-    let numbers: [i32; 6] = [1, 2, 3, 4, 5, 6];
-    println!("Array of numbers: {:?}", numbers);
-    println!("=======================");
-    let fruits: [&str; 4] = ["apple", "banana", "orange", "mango"];
-    println!("Array of fruits: {:?}", fruits);
-
-    // Tuples
-    let human= ("Phemi", 44, false);
-    println!("Human: {:?}", human)
-
+    let mut output: i32 = 20;
+    for _ in 0..1000 {
+        output += 100;
+    }
+    println!("The output: {}", output)
 }
